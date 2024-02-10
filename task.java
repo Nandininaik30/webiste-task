d5ce8b9531ef32f9b3fdac2a0aaf5aed83b283c3
Nandininaik30 <158474043+Nandininaik30@users.noreply.github.com>
2024-02-10 23:33:23 +0530

    Add files via upload

---
+++ task.java
@@ -0,0 +1,11 @@
+class pizza
+{
+    public static void main(String arg[])
+    {
+        large_pizza=8;
+        medium_pizza=6;
+        regular_pizza=4;
+        small_pizza=1;
+        
+    }
+}
